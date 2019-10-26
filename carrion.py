9f0019242fe961a9ca85fd7367cfc2e67b57916b
angel-robinson <57021550+angel-robinson@users.noreply.github.com>
2019-10-25 19:37:22 -0500

    Add files via upload

---
+++ carrion.py
@@ -0,0 +1,337 @@
+#trabajo03 variables
+mi_nombre_es="angel robinson"
+nº_dni=74286646
+primer_apellido="carrion"
+
+direccion="demetrio acosta"
+cuadra_nº=8
+departamento="lambayeque"
+
+donate_de_organos="False"
+codigo_poblacion=45353098
+serie=2
+
+ruc="1016650121"
+cliente="rosa"
+n_de_boleta="37038"
+
+nombre_de_la_madre="carmen"
+importe="S/.25.00"
+nombre_del_producto="guarda polvo"
+
+direccion_de_la_tienda="vicente de la vega #633"
+propietario="villavicencio sanchez"
+n_de_recibo="251-38839838"
+
+consultas=225534955
+conexion="monofasica"
+consumo="18 kwh"
+
+potencia="1.00 kw"
+inicio_de_contrato=31/10/2008
+termino_de_contrato=30/10/209
+
+fecha_de_vencimiento="11/10/2019"
+descuento=16.00
+total_a_pagar="S/.15.00"
+
+IGV=0.84
+alumbrado_publico="S/.0.42"
+pagina_web="www.distriluz.com.pe"
+
+dNI_del_cliente=16824742
+nombre_de_la_empresa="nor oriente"
+nombre_del_usuario="franquil"
+
+modulo="15"
+direccion_y_sentido="primer cuadrante"
+tema="vectores"
+
+temperatura="18°c"
+autor="a.venero"
+codigo_de_estudiante="14321d"
+
+matricula="S/.270"
+total="S/.380"
+pagina_unprg="www.unprg.edu.pe"
+
+
+nº_celular=980083183
+imei=12136168910213
+lanzamiento=2015
+
+inscripcion=13/10/2019
+promedio_final=14
+n_de_cursos=4
+
+placa="3145-d"
+chasis=13561
+color_vehiculo="rojo"
+
+modelo="l455-h"
+lugra_de_compra="chiclayo"
+partida=15328
+
+filial="chiclayo"
+boucher_n=123591
+agencia_de_pago="0233"
+
+tipo="nacional"
+departamento="lambayeque"
+provincia="chiclayo"
+
+A_egreso="2017"
+escuela="ingeneria electronica"
+turno="mañana"
+
+nº_sis=112
+ciclo="2019II"
+afiliado="sis"
+
+aula=2
+local="cpu chiclayo"
+ley_n=33790
+
+unidad="servicios"
+tension="736 v"
+hilos=5
+
+distrito="la victoria"
+descripcion="shampoo herbal"
+precio="S/,10.50"
+
+ugel="ugel chiclayo"
+nivel="secundaria"
+colegio_egresado="jorge basadre"
+
+grado="5to"
+seccion="A"
+sexo="masculino"
+
+emoji="☺"
+nombre_de_la_marca="pioneer"
+correo_electronico="carrioncruz01@gmail.com"
+
+contrasena="1956unprg"
+talla="1.61m"
+peso="62kg"
+
+gravedad_de_la_tierra="9.8m/s"
+formula_de_la_energia="e=mc"
+autor_de_la_teoria_de_la_relatividad="alberth einsten"
+
+sistema_operativo="android9.0"
+sistema_operativo_de_la_computadora="windows10 ultimate"
+nombre_del_videojuego="free fire"
+
+animal="gato"
+codigo_postal=25805
+pi=3.1416
+
+cancion="tremor"
+autor="matin garrix"
+genero="electronica"
+
+heroe_lambayecano="jose abelardo quiñones"
+mrta="movimiento revolucionario tupac amaru"
+lider_del_grupo_terrorista_sendero_luminoso="abimael Guzman"
+
+edad=17
+pago_de_alquiler="S/.180"
+intereses="s/.0.00"
+
+ubicacion_de_peru="america del sur"
+pais="peru"
+n_de_tarjeta="5632 8210 0005 5799"
+
+area_de_terreno="350 M2"
+forma_de_pago="efectivo"
+servicios="agua y desague"
+
+lengua="castellano"
+religion="catolico"
+estado_civil="soltero"
+
+n_hermanos="2"
+color_de_ojos="verdes"
+estudian="si"
+
+tienda="mifarma"
+tipo_de_documeto="dni"
+n_de_boleta="003562"
+
+hora_de_operacion="9:45:53"
+agente="farmancia x"
+nombre="smith sanchez"
+
+categoria_vehicular="dos b"
+clase_vehicular="A"
+n_de_licencia="c4568792"
+
+fecha_de_expedicion="29/03/2002"
+fecha_de_revalidacion="05/08/2018"
+fecha_de_nacimiento_del_condcutor="12/09/1988"
+
+grupo_sanguinio="O+"
+tipo_de_moneda="sol"
+ocupacion="ama de casa"
+
+pagina_web_del_banco_de_la_nacion="www.bn.com.pe"
+nombre_de_la_universidad="unprg"
+nombre_del_curso="programacion 1"
+
+operadora="movistar"
+tipo_de_archivo="zip"
+plan_contratado="20 mbps"
+
+tipo_de_red="4G"
+VPN="ninguna"
+direccion_ip="192.168.1.33"
+
+direccion_mac="04:1:67:62:d3:14"
+puerta_de_enlace="192.168.1.1"
+velocidad_d="72 mbps"
+
+programa="python"
+ruta="chiclayo lamabayeque"
+numero_de_telefono="224468"
+
+curso_1="basica 1"
+curso_2="analisis matematico"
+curso_3="tecnicas de estudio"
+
+fecha_y_ubigeo="130101"
+dni_de_la_madre="16463165"
+videojuego2="gta"
+
+nota=10
+aplicacion="spotify"
+nivel_de_bateria=80
+
+nombre="jose"
+curso="programacion"
+turno="mañana"
+
+grupo="a"
+universidad="unprg"
+profesor="Fernando"
+
+apellido_paterno="carrion"
+apellido_materno="cruz"
+segundo_nombre="robinson"
+
+peso="53"
+altura="1.57"
+sexo="masculino"
+
+costo="2.80"
+descuento="49.9"
+tienda="ripley"
+
+dias_de_la_semana="7"
+estudio="Falso"
+es_responsable="Falso"
+
+temperatura="22"
+pais="peru"
+lugar="costa"
+
+departamento="lambayeque"
+distrito="chiclayo"
+provincia="la victoria"
+
+dia_del_mes="21"
+numero_del_mes="10"
+dia="lunes"
+
+nro_de_cliente="500"
+nombre_de_la_tienda="moxxi"
+son_honestos="Verdadero"
+
+inicial_del_auto="200976.78"
+propietaria_del_auto="ana"
+responsable_con_las_cuotas="verdadero"
+
+marca_del_carro="toyota"
+cuotas_del_carro="25"
+color_del_carro="rojo"
+
+direccion="manuel seoane"
+cuadra="3"
+color_de_casa="azul"
+
+nro_de_hermanos="3"
+nro_de_tios="4"
+nro_de_primos="17"
+
+archivo="programacion"
+nro_de_documentos="10"
+nro_de_trabajos="03"
+
+#variables agrupadas de tres
+print("mi nombre es",mi_nombre_es, "mi primer apellido es",primer_apellido, "me indentifico con numero de dni",nº_dni)
+print("mi direccion actual es", direccion, "cuadra numero",cuadra_nº, "del departamento de",departamento)
+print("Es", donate_de_organos,"que dono organos" " y el codigo de poblacion es", codigo_poblacion,"serie", serie)
+print("el numero de ruc es",ruc,"el cliente es",cliente,"su numero de voleta es:",n_de_boleta)
+print("mi madre",nombre_de_la_madre,"me dio un importe de",importe,"para mi",nombre_del_producto)
+print("direcion de la tienda",direccion_de_la_tienda,"su propietario es",propietario,",el numero de recibo es",n_de_recibo)
+print("el codigo de consulta es:",consultas,"e alli hay wifi de conepcion tipo",conexion,"posee una corriente de",consumo)
+print("la potencia contratada es de",potencia,"cuyo inicio de contrato es",inicio_de_contrato,"y termina el",termino_de_contrato)
+print("la fecha de vencimiento es",fecha_de_vencimiento,"cuyo producto tiene un descuento de",descuento,"y el total de pago es de",total_a_pagar)
+print("el igv de un",IGV,"y el alumbrado publico es de un",alumbrado_publico,"para conocer mas puedes ingrear a la pagina web",pagina_web)
+print("el numero del dni de dicho cliente es",dNI_del_cliente,"este pertence a la empresa",nombre_de_la_empresa,"cuyo nombre es",nombre_del_usuario)
+print("el tema de analisis matematico es de",tema,"por ejemplo un vector puede estar en el",direccion_y_sentido,"con un modulo de",modulo)
+print("en lambayeque estamos a una temperatura de",temperatura,"el autor de la obra es",autor,"mi codigo de estudiante es",codigo_de_estudiante)
+print("la matricula del ciclo es",matricula,"pero el examen es de",total,"puedes ingresar a la pagina web",pagina_unprg)
+print("mi numero de telefono es",nº_celular,"con un imei",imei,"cuyo producto se lanzo en el",lanzamiento)
+print("lafechad de inscipcion fue el",inscripcion,"con un promedio final de",promedio_final,"el numero de curssos es de",n_de_cursos)
+print("el numero de placa",placa,"serie del chasis es",chasis,"el color del vehiculo es",color_vehiculo)
+print("el modelo de vehiculo es",modelo,"el lugra de compra es de la ciudad de",lugra_de_compra,"numero e partida es",partida)
+print("el numero de boucher es",boucher_n,"filial",filial,"y la agencia de pago es",agencia_de_pago)
+print("tipo",tipo,"del departamento de",departamento,"provincia",provincia)
+print("el año que egrese fue el",A_egreso,"a la escuela prof. de",escuela,"turno",turno)
+print("numero de sis es",nº_sis,"ciclo",ciclo,"afiliado al",afiliado)
+print("mi aula es",aula,"el local es",local,"y mi nº de ley es",ley_n)
+print("mi unidad es",unidad,"la tension es",tension,"el numero de hilos es",hilos)
+print("el distrito es",distrito,"uno el",descripcion,"y cuesta",precio)
+print("la ugel es",ugel,"mi nivel de estudios era",nivel,"el que colegio que termine mis estudios es",colegio_egresado)
+print("El grado es",grado,"la seccion",seccion,"mi tipo de sexo es",sexo)
+print("mi emoji favorito es",emoji,"en nombre de la marca se llama",nombre_de_la_marca,"mi correo personal es",correo_electronico)
+print("mi contraseña es",contrasena,"mi talla es",talla,"peso",peso)
+print("la gravedad de la tierra es",gravedad_de_la_tierra,"la formula de la energia es",formula_de_la_energia,"el inventor de la teoria de la relatividad es",autor_de_la_teoria_de_la_relatividad)
+print("el sistema es",sistema_operativo,"el sistema operativo de mi compu es",sistema_operativo_de_la_computadora,"mi juego favorito es",nombre_del_videojuego)
+print("mi animal favorito es",animal,"mi codigo postal es",codigo_postal,"el valor de pi es igual",pi)
+print("la cancion que mas escucho es",cancion,"el autor es",autor,"y es de tipo",genero)
+print("el heroe lambayecano es",heroe_lambayecano, "las iniciales de MRTA significa",mrta,"El lider del grupo sendero luminoso es",lider_del_grupo_terrorista_sendero_luminoso)
+print("mi edad es",17,"el alquiler de mi cuarto mensual es de",pago_de_alquiler,"el interes que me cobran es de",intereses)
+print("mi pais se encuentra ubicado es",ubicacion_de_peru,"se llama",pais,"mi numero de tarjeta es",n_de_tarjeta)
+print("el area del terreno que deseo comprar es de",area_de_terreno,"lo pagare en",forma_de_pago,"este terreno cuenta con",servicios)
+print("el lenguaje que utilizo para comunicarme es el",lengua,"soy de religion",religion,"mi estado civil es",estado_civil)
+print("mi numero de hermanos es",n_hermanos,"el color de mis ojos son",color_de_ojos,"ellos estudian:",estudian)
+print("realize mis compras en la farmacia",tienda,"con mi documento que es el",tipo_de_documeto,"el numero de comprobante",n_de_boleta)
+print("la hora que hice el deposito fue",hora_de_operacion,"en el agente de",agente,"a nombre de",nombre)
+print("la categoria behicular es",categoria_vehicular,"la clase vehicular es",clase_vehicular,"el numero de licencia es",n_de_licencia)
+print(" mi nombre es",nombre, ", y estoy en el curso de", curso , ", en el turno de la ", turno)
+print("soy del grupo", grupo, ", de la universidad", universidad ,", con el profesor", profesor)
+print("mi apellido paterno es ", apellido_paterno ,", y mi apeliido materno es ", apellido_materno ,", y llevo segundo nombre que es ", segundo_nombre)
+print("mi peso es ",peso," y mido ",altura," y el  sexo es ",sexo)
+print("el costo de un polo fue de ",costo,"y tuvo un descuento de ",descuento,"en la tienda",tienda)
+print("la semana tiene",dias_de_la_semana," dias , en la cual ser responsable es ",es_responsable," y ser estudioso es ",estudio)
+print("actualmente hay una temperatura de ",temperatura," grados en la ",lugar," del ",pais)
+print("vivo en el departamento ",departamento," ,en el distrito de ",distrito," ,en la provincia de ",provincia)
+print("el dia del mes de hoy dia es ",dia_del_mes,", del mes ",numero_del_mes,", del ",dia)
+print("en la tienda ",nombre_de_la_tienda," tiene una cantidad de ",nro_de_cliente," de clientes, y todos ellos son honestos, ",son_honestos)
+print("el inical de u auto es de ",inicial_del_auto," culla propietaria es ",propietaria_del_auto," ,y es responsable con sus cuotas ,",responsable_con_las_cuotas)
+print("un carro de la empresa ",marca_del_carro,", tiene",cuotas_del_carro," de pago , y es de color ",color_del_carro)
+print("vivo en la avenida ",direccion," ,en la cuadra ",str(cuadra),", en una casa de color ",color_de_casa)
+print("el total de hermanos somos ",str(nro_de_hermanos)," hermanos,con ", str(nro_de_tios)," tios ,y ",nro_de_primos," numeros de primos")
+print("el nombre de mi archivo se llama ",archivo," ,de lo cual tiene ",nro_de_documentos," documentos y ",nro_de_trabajos," trabajos")
+print("pertenesco al grupo sangunieo",grupo_sanguinio,"tipo de moneda",tipo_de_moneda,"ocupacion",ocupacion)
+print("hola,soy de la universidad nacional",nombre_de_la_universidad,"asisto a clases de",nombre_del_curso,"mas imfo a",pagina_web_del_banco_de_la_nacion)
+print("el tipo de archivo es",tipo_de_archivo,",operadora",operadora,"cuenta con una plan de",plan_contratado)
+print("tipo de red",tipo_de_red,"vpn",VPN,"direccion del ip",direccion_ip)
+print("direccion del mac",direccion_mac,"y la puerta de enlaces es,",puerta_de_enlace,"a una velocidad de",velocidad_d)
+print("este programa es",programa,"cuya ruta es",ruta,"numero de telefono",numero_de_telefono)
+print("los cursos que llebo son",curso_1,curso_2,curso_3)
+print("fecha de ubigeo",fecha_y_ubigeo,"el dni de mi madre es",dni_de_la_madre,"mi udeo juego favorito es",videojuego2)
+print("escucho musica en spotyfy",aplicacion,"siempre cuando mi movil esta en",nivel_de_bateria,"nota",nota)
